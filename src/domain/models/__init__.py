800db6c1e596aa65683de7d14bcd30a2ca892216
Phelipe Freire Pato <pheprogrammer@gmail.com>
2024-02-18 02:41:33 -0300

    Modularize models

---
+++ src/domain/models/__init__.py
@@ -0,0 +1,4 @@
+from domain.models.decode_data import DecodeData
+from domain.models.encode_data import EncodeData
+from domain.models.injection import Injections
+from domain.models.injections_payload import InjectionsPayload
